45b4292505436be44c93cf4793ac7cfe1764819f
Jonny K <Kempj2.jk@gmail.com>
2020-09-20 16:34:35 -0700

    got nomination pulling a lat long so that we can run the one call open weather request

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 console.log("connected")
+
+$(document).ready(function(){
+
+    var api = '17a9e463090b33048b8d0e143b013660'
+    var cityName = 'Seattle'
+    var lat
+    var long
+    $.get(`https://nominatim.openstreetmap.org/?q=${cityName}&format=json&limit=1`, function(response){
+        console.log(response)
+        lat = response[0].lat;
+        long = response[0].lon
+    }).then(function(){
+        $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
+        exclude=hourly,daily&appid=${api}`, function(response){
+            console.log(response);
+            var uvi = response.current.uvi
+        })
+        
+    })
+
+// $.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}`, function (response){
+//     console.log(response)
+//     console.log(response.name)
+    
+//     var date = moment.unix(response.dt).format('L')
+//     console.log(date)
+
+//     var temp = response.main.temp * (9/5) -459.67
+//     console.log(temp.toFixed(1));
+//     var humidity = response.main.humidity;
+//     var wind = response.wind.speed
+
+//     lat = response.coord.lat;
+//     long = response.coord.lon;
+
+//     // buildCurrentWeather(date, temp, humidity, wind)
+// }).then( function(){
+//     $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
+//     exclude=hourly,daily&appid=${api}`, function(response){
+//         console.log(response);
+//     })
+//     var uvi = response.current.uvi
+
+// })
+})
