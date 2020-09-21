b0432ad635ffa247c8864a99316ddb251fa669e0
Jonny K <Kempj2.jk@gmail.com>
2020-09-20 20:03:28 -0700

    better loop function for traversing the current weather dom and updating

--- script.js
+++ script.js
@@ -2,18 +2,23 @@
 $(document).ready(function(){
 
     var api = '17a9e463090b33048b8d0e143b013660'
-    var cityName = 'Pheonix'
+    var cityName = 'Vancouver'
     var lat
     var long
-    $.get(`https://nominatim.openstreetmap.org/?q=${cityName}&format=json&limit=1`, function(response){
+    $.get(`https://nominatim.openstreetmap.org/?q=${cityName}&addressdetails=1&countrycodes=US&format=json&limit=1`, function(response){
         console.log(response)
         lat = response[0].lat;
         long = response[0].lon;
+        cityName = `${cityName}, ${response[0].address.state}`
     }).then(function(){
         $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
         exclude=hourly,daily&appid=${api}`, function(response){
+            console.log(response)
             var currentWeather = {}
+            currentWeather.name = cityName
             currentWeather.date = moment.unix(response.current.dt).format('L'); 
+            currentWeather.icon = `http://openweathermap.org/img/wn/${response.current.weather.icon}.png`
+            currentWeather.desc = response.current.weather.description
             currentWeather.temp = (response.current.temp * (9/5) -459.67).toFixed(1);
             currentWeather.humidity = response.current.humidity;
             currentWeather.wind = response.current.wind_speed;
@@ -29,11 +34,18 @@ $(document).ready(function(){
         console.log(weather)
         for (var prop in weather){
             console.log(`${prop} : ${weather[prop]}`)
-            var target = $(`span[id=${prop}]`)
-            console.log(target)
-            target.text(weather[prop])
+           $(".currentWeather *").each(function(){
+               if($(this).attr("id") === prop){
+                   console.log($(this).attr("id"));
+                   console.log(prop)
+                    $(this).text(weather[prop])
+                    console.log($(this))
+               }
+           })
+                
+            // var target = $(`.currentWeather)[id=${prop}]`)
+            
         }
-        console.log($(".currentWeather"))
         
     }
 })
