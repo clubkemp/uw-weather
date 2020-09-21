15e99a9310e09eff1a606551e7bf7b71769dd3d6
Jonny K <Kempj2.jk@gmail.com>
2020-09-20 17:29:22 -0700

    inputing tags for targeting html

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("connected")
 
 $(document).ready(function(){
 
@@ -9,38 +8,24 @@ $(document).ready(function(){
     $.get(`https://nominatim.openstreetmap.org/?q=${cityName}&format=json&limit=1`, function(response){
         console.log(response)
         lat = response[0].lat;
-        long = response[0].lon
+        long = response[0].lon;
     }).then(function(){
         $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
         exclude=hourly,daily&appid=${api}`, function(response){
-            console.log(response);
-            var uvi = response.current.uvi
+            var currentWeather = {}
+            currentWeather.date = moment.unix(response.current.dt).format('L'); 
+            currentWeather.temp = (response.current.temp * (9/5) -459.67).toFixed(1);
+            currentWeather.humidity = response.current.humidity;
+            currentWeather.wind = response.current.wind_speed;
+            currentWeather.uvi = response.current.uvi;
+            buildCurrentWeather(currentWeather)
+            var fiveDayWeather = response.daily.slice(0,4)
+            //buildFiveDayWeather(fivedDayWeather)
         })
         
     })
 
-// $.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}`, function (response){
-//     console.log(response)
-//     console.log(response.name)
-    
-//     var date = moment.unix(response.dt).format('L')
-//     console.log(date)
-
-//     var temp = response.main.temp * (9/5) -459.67
-//     console.log(temp.toFixed(1));
-//     var humidity = response.main.humidity;
-//     var wind = response.wind.speed
-
-//     lat = response.coord.lat;
-//     long = response.coord.lon;
-
-//     // buildCurrentWeather(date, temp, humidity, wind)
-// }).then( function(){
-//     $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
-//     exclude=hourly,daily&appid=${api}`, function(response){
-//         console.log(response);
-//     })
-//     var uvi = response.current.uvi
-
-// })
+    function buildCurrentWeather (weather) {
+        console.log(weather)
+    }
 })
